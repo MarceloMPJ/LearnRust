6a5d1fa87473443ce51ed9ef436bf248b6c22e23
Marcelo Junior <marcelo.jr63@gmail.com>
2021-05-10 21:25:28 -0300

    Adicionando loop para o Jogo da Adivinhação

--- jogo_adivinhacao/src/main.rs
+++ jogo_adivinhacao/src/main.rs
@@ -5,25 +5,28 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-  println!("Adivinhe o número! (Modo SUPER DIFICIL!)");
+  println!("Adivinhe o número!");
 
   let numero_secreto = rand::thread_rng().gen_range(1..100);
 
-  println!("O número secreto é: {}", numero_secreto);
+  loop {
+    println!("Digite um número (1 - 100):");
 
-  println!("Nossa, qual será o número secreto??? Digite um número:");
+    let mut palpite = String::new();
 
-  let mut palpite = String::new();
+    io::stdin().read_line(&mut palpite)
+      .expect("Falha ao ler a entrada!");
 
-  io::stdin().read_line(&mut palpite)
-    .expect("Falha ao ler a entrada!");
+    let palpite: u32 = palpite.trim().parse()
+      .expect("Digite um número!");
 
-  let palpite: u32 = palpite.trim().parse()
-    .expect("Digite um número!");
-
-  match palpite.cmp(&numero_secreto) {
-    Ordering::Less => println!("Muito baixo!"),
-    Ordering::Greater => println!("Muito alto!"),
-    Ordering::Equal => println!("ACERTOOOOOOOOOU!")
+    match palpite.cmp(&numero_secreto) {
+      Ordering::Less => println!("Muito baixo!"),
+      Ordering::Greater => println!("Muito alto!"),
+      Ordering::Equal => {
+        println!("ACERTOOOOOOOOOU!");
+        break;
+      }
+    }
   }
 }
